1618b30da4cc04f34b5f7e5629160cc077854855
kennck <kennettecanales@gmail.com>
2023-06-11 23:18:34 +0800

    add register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -11,46 +11,44 @@ function page(props) {
             <div className='bg-black dark:bg-white h-[100vh] float-left text-white dark:text-black lg:w-1/4 2xl:w-1/4  sm:w-full md:w-full xs:w-full'>
                 <section className='mt-[250px] text-center lg:px-[20px] 2xl:px-[20px] sm:px-[50px] xs:px-[50px] md:px-[50px]'>
                     <p className='text-lg text-white dark:text-black mb-[50px]'>
-                        Login
+                        Register
                     </p>
                     <TextField
                         type="text"
-                        placeholder="Username or Email Address"
+                        placeholder="Email Address"
                     />
 
                     <TextField
                         type="password"
-                        placeholder="********"
+                        placeholder="Password"
                     />
 
-                    <span 
-                        onClick={() => {
-                            router.push('/forgetpassword')
-                        }}
-                        className='w-full h-[50px] float-left hover:font-bold cursor-pointer mt-[50px]'>
-                        Forget your password?
-                    </span>
+                    <TextField
+                        type="password"
+                        placeholder="Confirm Password"
+                    />
+
                     <Button
-                        title="Login"
+                        title="Register"
                         onClick={() => {
                             //
                         }}
                     />
 
                     <span className='w-full h-[50px] float-left mt-[50px]'>
-                        Don't have an account? 
-                        <span 
+                        Have an account?
+                        <span
                             onClick={() => {
-                                router.push('/register')
+                                router.push('/login')
                             }}
-                            className='font-bold cursor-pointer px-2'>Register</span>
+                            className='font-bold cursor-pointer px-2'>Login</span>
                     </span>
                 </section>
             </div>
             <div className='bg-white dark:bg-black h-[100vh] float-left text-black dark:text-white lg:w-3/4 2xl:w-3/4 sm:w-full md:w-full xs:w-full'>
                 <div className='flex items-center content-center h-[100vh] w-full px-[50px]'>
                     <section>
-                        <p className='text-6xl text-black dark:text-white font-bold w-full float-left'>Continue Building Your Thing</p>
+                        <p className='text-6xl text-black dark:text-white font-bold w-full float-left'>Let's Build the Startup Ecosystem in the Philippines</p>
                         <ul className='text-2xl text-black dark:text-white w-full float-left mt-[50px]'>
                             <li className='float-left w-full'>Share your Deck and build your profile </li>
                             <li className='float-left w-full'>Grow your network easily</li>
