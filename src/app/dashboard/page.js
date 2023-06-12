c906aa3cfa2e5ed30f75ea239db7fa522b955565
kennck <kennettecanales@gmail.com>
2023-06-12 10:48:11 +0800

    update

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,12 +7,12 @@ function page(props) {
     return (
         <div className='float-left w-full min-h-full bg-white dark:bg-black text-black dark:text-white'>
             <Header />
-            <div className='w-full min-h-[100vh] bg-gray-100 float-left'>
-                <div className='lg:block 2xl:block sm:hidden xs:hidden md:hidden w-[15%] min-h-[100vh] float-left'>
+            <div className='w-full min-h-[100vh] bg-gray-100 float-left relative'>
+                <div className='lg:block 2xl:block sm:hidden xs:hidden md:hidden w-[15%] min-h-[100vh] fixed float-left'>
                     <Sidebar />
                 </div>
                 <div
-                    className='lg:w-[85%] min-h-[100vh] 2xl:w-[85%] sm:w-full xs:w-full md:w-full float-left p-[20px] mt-[80px]'>
+                    className='lg:w-[85%] min-h-[100vh] 2xl:w-[85%] sm:w-full xs:w-full md:w-full float-left p-[20px] mt-[80px] lg:ml-[15%] 2xl:ml-[15%]'>
                     <div className='w-full float-left lg:flex 2xl:flex justify-between'>
                         <LineChart />
                         <TableChart title="Startup 2023"/>
