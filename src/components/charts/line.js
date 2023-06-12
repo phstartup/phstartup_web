b9ecb16231a2a05735ba322ab4043f29c63326f7
kennck <kennettecanales@gmail.com>
2023-06-12 12:44:01 +0800

    initial setup

--- src/components/charts/line.js
+++ src/components/charts/line.js
@@ -4,9 +4,6 @@ import Header from './header';
 function body(props) {
     return (
         <div
-            style={{
-                border: 'solid 1px #eee'
-            }}
             class="mx-auto lg:w-[49%] 2xl:w-[49%] sm:w-full xs:w-full md:w-full h-[400px] float-left bg-white dark:bg-gray-800 rounded-[10px] px-[20px] mb-[20px]">
                 <Header title="Total Funding 2023" rightTitle={props.rightTitle}/>
                 <div className='w-full float-left h-[calc(400px - 80px)]'>
