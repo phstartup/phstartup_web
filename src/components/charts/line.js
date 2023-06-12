028561ee85bf1ad1b103bcba147c0c982203497a
kennck <kennettecanales@gmail.com>
2023-06-12 12:22:27 +0800

    updte

--- src/components/charts/line.js
+++ src/components/charts/line.js
@@ -7,7 +7,7 @@ function body(props) {
             style={{
                 border: 'solid 1px #eee'
             }}
-            class="mx-auto lg:w-[49%] 2xl:w-[49%] sm:w-full xs:w-full md:w-full h-[400px] float-left bg-white rounded-[10px] px-[20px] mb-[20px]">
+            class="mx-auto lg:w-[49%] 2xl:w-[49%] sm:w-full xs:w-full md:w-full h-[400px] float-left bg-white dark:bg-gray-800 rounded-[10px] px-[20px] mb-[20px]">
                 <Header title="Total Funding 2023" rightTitle={props.rightTitle}/>
                 <div className='w-full float-left h-[calc(400px - 80px)]'>
                     <span className='text-6xl'>
