cb288ccac2b97a33f0a88842422aeccd4aff54e6
kennck <kennettecanales@gmail.com>
2023-08-01 16:28:21 +0800

    update

---
+++ src/app/incubators/page.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import Pitches from '../pitch/page'
+
+function page(props) {
+  return (
+    <div className="w-full float-left">
+      <Pitches />
+    </div>
+  );
+}
+
+export default page;
