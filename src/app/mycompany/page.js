a46652f778a85fee53cbe3f752fa7185aefeeaac
kennck <kennettecanales@gmail.com>
2023-08-03 22:25:48 +0800

    update

--- src/app/mycompany/page.js
+++ src/app/mycompany/page.js
@@ -9,6 +9,74 @@ import Team from '@/components/mycompany/team'
 import { useSession } from 'next-auth/react';
 import Api from '@/lib/api';
 let api = new Api()
+
+
+const team = [{
+    username: 'kennette',
+    information: {
+        position: 'CEO, Increment Technologies Inc.',
+        first_name: 'Kennette',
+        last_name: 'Canales',
+        profile: 'https://lh3.googleusercontent.com/a/AAcHTtcb9ye9Utq3d9PI1SSylYY4BlHdW04S0bcqkrx67sIKhzY=s96-c',
+        social: {
+            facebook: 'https://facebook.com',
+            linkedIn: 'https://linkedin.com'
+        },
+        contact_number: '09123456789',
+        email: 'kenn@jiph.co',
+        about: 'Software Engineer'
+    },
+    id: 1
+}, {
+    username: 'kennette',
+    information: {
+        position: 'CEO, Increment Technologies Inc.',
+        first_name: 'Kennette',
+        last_name: 'Canales',
+        profile: 'https://lh3.googleusercontent.com/a/AAcHTtcb9ye9Utq3d9PI1SSylYY4BlHdW04S0bcqkrx67sIKhzY=s96-c',
+        social: {
+            facebook: 'https://facebook.com',
+            linkedIn: 'https://linkedin.com'
+        },
+        contact_number: '09123456789',
+        email: 'kenn@jiph.co',
+        about: 'Software Engineer'
+    },
+    id: 1
+}, {
+    username: 'kennette',
+    information: {
+        position: 'CEO, Increment Technologies Inc.',
+        first_name: 'Kennette',
+        last_name: 'Canales',
+        profile: 'https://lh3.googleusercontent.com/a/AAcHTtcb9ye9Utq3d9PI1SSylYY4BlHdW04S0bcqkrx67sIKhzY=s96-c',
+        social: {
+            facebook: 'https://facebook.com',
+            linkedIn: 'https://linkedin.com'
+        },
+        contact_number: '09123456789',
+        email: 'kenn@jiph.co',
+        about: 'Software Engineer'
+    },
+    id: 1
+}, {
+    username: 'kennette',
+    information: {
+        position: 'CEO, Increment Technologies Inc.',
+        first_name: 'Kennette',
+        last_name: 'Canales',
+        profile: 'https://lh3.googleusercontent.com/a/AAcHTtcb9ye9Utq3d9PI1SSylYY4BlHdW04S0bcqkrx67sIKhzY=s96-c',
+        social: {
+            facebook: 'https://facebook.com',
+            linkedIn: 'https://linkedin.com'
+        },
+        contact_number: '09123456789',
+        email: 'kenn@jiph.co',
+        about: 'Software Engineer'
+    },
+    id: 1
+}]
+
 function page(props) {
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(false)
@@ -20,10 +88,13 @@ function page(props) {
 
 
     const getData = async () => {
-        if(!session) return
+        if (!session) return
         setLoading(true)
         await api.get('/api/companies?user_id=' + session.user.id, session?.accessToken, (response) => {
-            setData(response.data)
+            setData({
+                ...response.data,
+                team
+            })
             setTimeout(() => {
                 setLoading(false)
             }, 1000)
@@ -40,12 +111,12 @@ function page(props) {
             {
                 !loading && (
                     <div className='w-full float-left mt-[20px]'>
-                        <Banner data={data} getData={() => getData()}/>
-                        <Information data={data} getData={() => getData()}/>
-                        {/* <Team /> */}
-                        <Pitches data={data} getData={() => getData()}/>
-                        <Services data={data} getData={() => getData()}/>
-                        <Achievements  data={data} getData={() => getData()}/>
+                        <Banner data={data} getData={() => getData()} />
+                        <Information data={data} getData={() => getData()} />
+                        <Team data={data} getData={() => getData()} />
+                        <Pitches data={data} getData={() => getData()} />
+                        <Services data={data} getData={() => getData()} />
+                        <Achievements data={data} getData={() => getData()} />
                     </div>
                 )
             }
--- src/components/mycompany/team.js
+++ src/components/mycompany/team.js
@@ -5,9 +5,14 @@ import Button from '@/components/buttons/btn'
 import Modal from '@/components/modal/index'
 import TextInput from '@/components/form/text';
 import TextArea from '@/components/form/textarea'
+import { SvgIcon } from '@mui/material';
+import { Face6, Facebook, LinkedIn } from '@mui/icons-material';
+import Helper from '@/lib/helper';
+let helper = new Helper()
 
 function Team(props) {
     const [createFlag, setCreateFlag] = useState(false)
+    const { data } = props
 
     const renderContent = () => {
         return (
@@ -41,6 +46,43 @@ function Team(props) {
     }
 
 
+    const renderProfile = (user) => {
+        let size = 100
+        let radius = size / 2
+        return (
+            <div
+                className={`h-[${size}px] w-[${size}px] rounded-[${radius}px] mr-[10px] float-left relative`}
+            >
+                {
+                    (user.information && user.information.profile) ? (
+                        <img
+                            src={user.information.profile}
+                            width={size}
+                            height={size}
+                            style={{
+                                borderRadius: radius
+                            }}
+                            className={`rounded-[${radius}px] cursor-pointer border-2 border-green-500 dark:border-green-500`}
+                            alt={user.information.profile}
+                        />
+                    ) : (
+                        <div className={`h-[${size}px] w-[${size}px] rounded-[${radius}px] bg-green-400 dark:bg-white float-left cursor-pointer`}>
+                            <SvgIcon component={Face6}
+                                className='text-white dark:text-black'
+                                style={{
+                                    fontSize: size
+                                }}
+                            />
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
+
+
+
+
     return (
         <div className={Style.cardContainer}>
             <div className='flex w-full items-center content-center justify-between'>
@@ -66,6 +108,73 @@ function Team(props) {
             </div>
 
             <div className='w-full float-left text-sm mt-[20px]'>
+
+                {
+                    data && data.team && data.team.map((item, index) => (
+                        <div
+                            key={index}
+                            onClick={() => {
+                            }}
+                            className={'float-left h-[200px] rounded-lg border border-gray-100 dark:border-gray-700 cursor-pointer mb-[20px] lg:w-[24%] xl:w-[24%] 2xl:w-[24%] sm:w-[100%] xs:w-[100%] md:w-[49%] lg:mr-[1%] xl:mr-[1%] 2xl:mr-[1%] md:mr-[1%] xs:mr-[0%] sm:mr-[0%] p-[10px]'}
+                        >
+                            <div className='w-full float-left'>
+                                <span className='float-left w-[30%]'>
+                                    {
+                                        renderProfile(item)
+                                    }
+                                </span>
+                                <span className='float-left w-[70%] h-[100px] flex items-center content-center' >
+                                    <span className='w-full float-left  pl-[10px]'>
+                                        <span className='font-semibold float-left w-full'>
+                                            {
+                                                helper.getName(item)
+                                            }
+                                        </span>
+                                        <span className='float-left w-full'>
+                                            {
+                                                item.information.position
+                                            }
+                                        </span>
+                                        {
+                                            item.information && item.information.social && (
+                                                <span className='float-left w-full mt-[10px]'>
+                                                    {
+                                                        item.information.social.facebook && (
+                                                            <SvgIcon
+                                                                onClick={() => {
+                                                                    window.open(item.information.social.facebook, '_blank')
+                                                                }}
+                                                                component={Facebook}
+                                                            />
+                                                        )
+                                                    }
+
+                                                    {
+                                                        item.information.social.linkedIn && (
+                                                            <SvgIcon
+                                                                onClick={() => {
+                                                                    window.open(item.information.social.linkedIn, '_blank')
+                                                                }}
+                                                                component={LinkedIn}
+                                                            />
+                                                        )
+                                                    }
+
+                                                </span>
+                                            )
+                                        }
+
+                                    </span>
+                                </span>
+                            </div>
+                            <span className='float-left w-full text-xs mt-[20px] text-justify mb-[20px] overflow-ellipsis'>
+                                {
+                                    item.information.about
+                                }
+                            </span>
+                        </div>
+                    ))
+                }
             </div>
 
             {
