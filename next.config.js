da008d352c1ad68798660352a4e70362f46e8233
kennck <kennettecanales@gmail.com>
2023-08-04 19:22:36 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'dist',
     modularizeImports: {
         "@mui/material": {
             transform: "@mui/material/{{member}}"
