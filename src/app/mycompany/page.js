99ceeec1a0922abb806faa1fb1690f07619ca084
kennck <kennettecanales@gmail.com>
2023-08-14 18:27:52 +0800

    update

--- src/app/mycompany/page.js
+++ src/app/mycompany/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import Achievements from '@/components/mycompany/Achievements';
 import Information from '@/components/mycompany/information';
 import Banner from '@/components/mycompany/Banner';
@@ -11,105 +11,67 @@ import FetchLoading from '@/components/loading/SpinnerWhole'
 import Api from '@/lib/api';
 let api = new Api()
 
-const withSession = (Component) => (props) => {
-    const session = useSession()
+function MyCompany(props) {
+    const {data:session} = useSession()
+    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState(null)
 
-    // if the component has a render property, we are good
-    if (Component.prototype.render) {
-        return <Component session={session} {...props} />
-    }
+    useEffect(() => {
+        getData()
+    }, [])
 
-    // if the passed component is a function component, there is no need for this wrapper
-    throw new Error(
-        [
-            "You passed a function component, `withSession` is not needed.",
-            "You can `useSession` directly in your component.",
-        ].join("\n")
-    )
-}
-
-class Page extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            data: null,
-            loading: null
-        };
-    }
-
-    async componentDidMount() {
-        this.getData()
-    }
-
-
-    getData = async () => {
-        const { data: session } = this.props.session;
+    const getData = async () => {
         if (!session) return
-        this.setState({
-            loading: true
-        })
+        setLoading(true)
         await api.get('/api/companies?user_id=' + session.user.id, session?.accessToken, (response) => {
             if (response.data) {
-                this.setState({
-                    data: response.data
-                })
+                setData(response.data)
             }
             setTimeout(() => {
-
-                this.setState({
-                    loading: false
-                })
+                setLoading(false)
             }, 1000)
         }, (error) => {
             setTimeout(() => {
-
-                this.setState({
-                    loading: false
-                })
+                setLoading(false)
             }, 1000)
 
         })
     }
 
+    return (
+        <div className='w-full float-left' >
+            {
+                !loading && (
+                    <div className='w-full float-left mt-[20px]'>
+                        {
+                            data && (
+                                <Banner data={data} getData={() => getData()} />
+                            )
+                        }
 
-    render() {
-        const { loading, data } = this.state;
-        const { data: session } = this.props.session
+                        <Information data={data} getData={() => getData()} />
 
-        return (
-            <div className='w-full float-left' >
-                {
-                    !loading && (
-                        <div className='w-full float-left mt-[20px]'>
-                            {
-                                data && (
-                                    <Banner data={data} getData={() => this.getData()} />
-                                )
-                            }
+                        {
+                            data && (
+                                <div className='w-full float-left'>
+                                    <Team data={data} getData={() => getData()} />
+                                    <Pitches data={data} getData={() => getData()} />
+                                    <Services data={data} getData={() => getData()} />
+                                    <Achievements data={data} getData={() => getData()} />
+                                </div>
+                            )
+                        }
 
-                            <Information data={data} getData={() => this.getData()} />
-
-                            {
-                                data && (
-                                    <div className='w-full float-left'>
-                                        <Team data={data} getData={() => this.getData()} />
-                                        <Pitches data={data} getData={() => this.getData()} />
-                                        <Services data={data} getData={() => this.getData()} />
-                                        <Achievements data={data} getData={() => this.getData()} />
-                                    </div>
-                                )
-                            }
-
-                        </div>
-                    )
-                }
-                {
-                    loading && (
-                        <FetchLoading></FetchLoading>
-                    )
-                }
-            </div >)
-    };
+                    </div>
+                )
+            }
+            {
+                loading && (
+                    <FetchLoading></FetchLoading>
+                )
+            }
+        </div >
+    )
 }
 
-export default withSession(Page);
+export default MyCompany;
